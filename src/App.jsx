b8e869f5baf0a39e8ebad57c5c72a392d9b9d189
awbtravels <awbtravelsntours@outlook.com>
2025-06-19 00:58:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// src/App.jsx import React from "react"; import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage"; import AboutPage from "./pages/AboutPage"; // When ready, import other pages like this: // import ContactPage from "./pages/ContactPage"; // import FlightsPage from "./pages/FlightsPage"; // import HotelsPage from "./pages/HotelsPage"; // import InsurancePage from "./pages/InsurancePage"; // import ServicesPage from "./pages/ServicesPage"; // import NewsPage from "./pages/NewsPage"; // import ConsultationPage from "./pages/ConsultationPage";
 
-// Placeholder pages – You can create these files later in ./pages folder
-const FlightsPage = () => <div className="p-8 text-charcoal">Flights Page Coming Soon</div>;
-const HotelsPage = () => <div className="p-8 text-charcoal">Hotels Page Coming Soon</div>;
-const InsurancePage = () => <div className="p-8 text-charcoal">Insurance Page Coming Soon</div>;
-const ConsultationPage = () => <div className="p-8 text-charcoal">Consultation Page Coming Soon</div>;
-const ServicesPage = () => <div className="p-8 text-charcoal">Services Page Coming Soon</div>;
-const NewsPage = () => <div className="p-8 text-charcoal">News & Travel Tips Coming Soon</div>;
-const ContactPage = () => <div className="p-8 text-charcoal">Contact Page Coming Soon</div>;
+export default function App() { return ( <Router> <Routes> <Route path="/" element={<HomePage />} /> <Route path="/about" element={<AboutPage />} />
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/flights" element={<FlightsPage />} />
-        <Route path="/hotels" element={<HotelsPage />} />
-        <Route path="/insurance" element={<InsurancePage />} />
-        <Route path="/consultation" element={<ConsultationPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </Router>
-  );
-}
+{/* Uncomment and add these when the pages are ready */}
+    {/* <Route path="/contact" element={<ContactPage />} /> */}
+    {/* <Route path="/flights" element={<FlightsPage />} /> */}
+    {/* <Route path="/hotels" element={<HotelsPage />} /> */}
+    {/* <Route path="/insurance" element={<InsurancePage />} /> */}
+    {/* <Route path="/services" element={<ServicesPage />} /> */}
+    {/* <Route path="/news" element={<NewsPage />} /> */}
+    {/* <Route path="/consultation" element={<ConsultationPage />} /> */}
+  </Routes>
+</Router>
 
-export default App;
+); }
