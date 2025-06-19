e9052d20838ef711890b19d4a1ee478ffed79f00
awbtravels <awbtravelsntours@outlook.com>
2025-06-19 22:51:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 // src/App.jsx
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
+// Page Components (You can create these files under /pages if missing)
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Consultation from "./pages/Consultation";
+import Insurance from "./pages/Insurance";
+import Team from "./pages/Team";
+import News from "./pages/News";
+import Flights from "./pages/Flights";
+import Hotels from "./pages/Hotels";
+import Services from "./pages/Services";
+
 export default function App() {
-  return <Layout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="consultation" element={<Consultation />} />
+          <Route path="insurance" element={<Insurance />} />
+          <Route path="team" element={<Team />} />
+          <Route path="news" element={<News />} />
+          <Route path="flights" element={<Flights />} />
+          <Route path="hotels" element={<Hotels />} />
+          <Route path="services" element={<Services />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
