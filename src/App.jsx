de51bf6ea09b98a77896824174ad3312f29e1602
awbtravels <awbtravelsntours@outlook.com>
2025-06-19 03:02:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import TeamPage from "./pages/TeamPage"; // ✅ New team page
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/team" element={<TeamPage />} /> {/* ✅ Team route */}
       </Routes>
     </Router>
   );
