e965437bf436aef89ef06f2de2f5b1179b746405
awbtravels <awbtravelsntours@outlook.com>
2025-06-20 14:17:04 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,25 +14,3 @@ import AboutPage from "./pages/AboutPage";
 import Team from "./pages/Team";
 import NewsPage from "./pages/NewsPage";
 import TestimonialsPage from "./pages/TestimonialsPage";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/consultation" element={<ConsultationPage />} />
-          <Route path="/consultation-success" element={<ConsultationSuccess />} />
-          <Route path="/flights" element={<Flights />} />
-          <Route path="/hotels" element={<Hotels />} />
-          <Route path="/insurance" element={<Insurance />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/testimonials" element={<TestimonialsPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
