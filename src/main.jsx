97073bcfc1a940a5d96566fac54e8b69d6b3d70c
awbtravels <awbtravelsntours@outlook.com>
2025-06-20 16:52:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import "./index.css"; // ✅ Import Tailwind CSS styles here
 
+// Page components
 import HomePage from "./pages/HomePage";
 import ConsultationPage from "./pages/ConsultationPage";
 import ConsultationSuccess from "./pages/ConsultationSuccess";
@@ -12,7 +14,7 @@ import Hotels from "./pages/Hotels";
 import Insurance from "./pages/Insurance";
 import ServicesPage from "./pages/ServicesPage";
 import AboutPage from "./pages/AboutPage";
-import Team from "./pages/TeamPage";
+import Team from "./pages/TeamPage"; // ✅ Corrected name
 import NewsPage from "./pages/NewsPage";
 import TestimonialsPage from "./pages/TestimonialsPage";
 
