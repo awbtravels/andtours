554ee59d4ffa0cbd37b3c746c5ad9e7a09656df2
awbtravels <awbtravelsntours@outlook.com>
2025-06-20 11:52:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App";
 
-// Pages
-import HomePage from "./pages/HomePage.jsx";
-import ConsultationPage from "./pages/ConsultationPage.jsx";
-import ConsultationSuccess from "./pages/ConsultationSuccess.jsx";
-import Flights from "./pages/Flights.jsx";
-import Hotels from "./pages/Hotels.jsx";
-import Insurance from "./pages/Insurance.jsx";
-import ServicesPage from "./pages/ServicesPage.jsx";
-import AboutPage from "./pages/AboutPage.jsx";
-import Team from "./pages/Team.jsx";
-import NewsPage from "./pages/NewsPage.jsx";
+// Page Components
+import HomePage from "./pages/HomePage";
+import ConsultationPage from "./pages/ConsultationPage";
+import ConsultationSuccess from "./pages/ConsultationSuccess";
+import Flights from "./pages/Flights";
+import Hotels from "./pages/Hotels";
+import Insurance from "./pages/Insurance";
+import ServicesPage from "./pages/ServicesPage";
+import AboutPage from "./pages/AboutPage";
+import Team from "./pages/Team";
+import NewsPage from "./pages/NewsPage";
+import TestimonialsPage from "./pages/TestimonialsPage"; // ✅ NEW
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -21,15 +23,16 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          <Route path="consultation" element={<ConsultationPage />} />
-          <Route path="consultation-success" element={<ConsultationSuccess />} />
-          <Route path="flights" element={<Flights />} />
-          <Route path="hotels" element={<Hotels />} />
-          <Route path="insurance" element={<Insurance />} />
-          <Route path="services" element={<ServicesPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="team" element={<Team />} />
-          <Route path="news" element={<NewsPage />} />
+          <Route path="/consultation" element={<ConsultationPage />} />
+          <Route path="/consultation-success" element={<ConsultationSuccess />} />
+          <Route path="/flights" element={<Flights />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/insurance" element={<Insurance />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/testimonials" element={<TestimonialsPage />} /> {/* ✅ NEW */}
         </Route>
       </Routes>
     </BrowserRouter>
