205b3d6edead698bc98148e7c9fe4e0367084fec
awbtravels <awbtravelsntours@outlook.com>
2025-06-21 18:10:39 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,10 @@
+// src/pages/HomePage.jsx
 import React from "react";
 import TestimonialsWithSlider from "../components/TestimonialsWithSlider";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-white">
-      <h1 className="text-3xl font-bold mb-8 text-center text-red-primary pt-6">
-        Welcome to AWB Travels and Tours
-      </h1>
-
-      {/* Testimonials and Image Slider Section */}
+    <div>
       <TestimonialsWithSlider />
     </div>
   );
